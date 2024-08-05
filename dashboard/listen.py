fbbe19a8e045f9fdbed96cc1d98ce86f7a1095c1
Stephen Hulme <sh51@sanger.ac.uk>
2024-08-05 15:39:37 +0100

    Gracefully handle connection errors when listening

--- dashboard/listen.py
+++ dashboard/listen.py
@@ -1,8 +1,10 @@
 import time
+from datetime import datetime
+
+import applications
 import db
 import gitlab
-import applications
-from datetime import datetime
+import requests
 from serve import app
 
 
@@ -12,24 +14,29 @@ def listen(timeout=300):
             print(
                 f"[{  datetime.now().strftime('%Y-%m-%d %H:%M:%S')    }] Querying for new pipelines"
             )
-            pipelines = gitlab.query_pipelines()
+            try:
+                pipelines = gitlab.query_pipelines()
 
-            # if first pipeline id is not in database, run the new_pipeline function
-            latest_pipeline_id = pipelines[0]["id"].split("/")[-1]
-            if not db.get_versions(latest_pipeline_id):
+                # if first pipeline id is not in database, run the new_pipeline function
+                latest_pipeline_id = pipelines[0]["id"].split("/")[-1]
+                if not db.get_versions(latest_pipeline_id):
 
-                print(
-                    f" * New pipeline detected ({latest_pipeline_id}), capturing versions"
-                )
+                    print(
+                        f" * New pipeline detected ({latest_pipeline_id}), capturing versions"
+                    )
 
-                lb_version = applications.fetch_version(
-                    "https://uat.limber.psd.sanger.ac.uk/", ".version-info .container"
-                )
-                ss_version = applications.fetch_version(
-                    "https://uat.sequencescape.psd.sanger.ac.uk/", ".deployment-info"
-                )
+                    lb_version = applications.fetch_version(
+                        "https://uat.limber.psd.sanger.ac.uk/",
+                        ".version-info .container",
+                    )
+                    ss_version = applications.fetch_version(
+                        "https://uat.sequencescape.psd.sanger.ac.uk/",
+                        ".deployment-info",
+                    )
 
-                db.add_versions(latest_pipeline_id, ss_version, lb_version)
+                    db.add_versions(latest_pipeline_id, ss_version, lb_version)
+            except requests.exceptions.ConnectionError:
+                print(" * Could not connect to GitLab, are you on the VPN?")
 
             time.sleep(timeout)
 
